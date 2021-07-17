548d40c75f2ecc6fd78f40c31d3aa7c15b3bd2c0
Hosamani <vinod.hosamani@harman.com>
2021-07-17 19:00:56 +0530

    removed one line

--- Simple.java
+++ Simple.java
@@ -5,7 +5,6 @@ public class Simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("hi everyone");
-		System.out.println("don");
 
 	}
 
