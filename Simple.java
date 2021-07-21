f0609e4f21977b9f86c6523b4b8ca9b1c4645d89
Hosamani <vinod.hosamani@harman.com>
2021-07-21 19:31:28 +0530

    added code to main branch

--- Simple.java
+++ Simple.java
@@ -4,8 +4,9 @@ public class Simple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Sysout.out.println("this is added code in main branch");
         System.out.println("hi everyone");
-
+           
 	}
 
 }
