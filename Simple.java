9f5a56b87de4b95218f51c744baef2f273c540b0
Hosamani <vinod.hosamani@harman.com>
2021-07-21 20:11:23 +0530

    dfdf

--- Simple.java
+++ Simple.java
@@ -4,9 +4,15 @@ public class Simple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Sysout.out.println("this is added code in main branch");
-        System.out.println("hi everyone");
-           
+		
+
+		Sysout.out.println("its thirst branch");
+		 System.out.println("hi everyone");
+		Sysout.out.println("its thirst branch added some lines of code");
+
+        Sysout.out.println("this is added code in main branch");
+       
+
 	}
 
 }
